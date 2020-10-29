98fbd9169fca886dc1400e8e909bcc7663d0e4c1
Ravindi Kumarasinghe <57246252+ravindikumarasinghe@users.noreply.github.com>
2020-10-29 23:59:12 +0530

    modify code

--- app/src/main/java/com/example/calculateyourtip/MainActivity.java
+++ app/src/main/java/com/example/calculateyourtip/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button b1;
     TextView rs;
+    EditText n1,n2;
 
 
     @Override
