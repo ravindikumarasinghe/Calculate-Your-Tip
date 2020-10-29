8f16e0f498564d69356cfadf4c2a25b0aa851168
Ravindi Kumarasinghe <57246252+ravindikumarasinghe@users.noreply.github.com>
2020-10-29 23:58:20 +0530

    modify code

--- app/src/main/java/com/example/calculateyourtip/MainActivity.java
+++ app/src/main/java/com/example/calculateyourtip/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.calculateyourtip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
