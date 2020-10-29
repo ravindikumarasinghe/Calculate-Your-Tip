4e4ded981b2f924471089f69c7ab9c0be1aabc08
Ravindi Kumarasinghe <57246252+ravindikumarasinghe@users.noreply.github.com>
2020-10-30 00:07:20 +0530

    modify code

--- app/src/main/java/com/example/calculateyourtip/MainActivity.java
+++ app/src/main/java/com/example/calculateyourtip/MainActivity.java
@@ -19,5 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.button);
     }
 }
